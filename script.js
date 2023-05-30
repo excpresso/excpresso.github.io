8e2d63021cb8e38d6e6f18d3d7ea2e0041b7389c
excpresso <131495947+excpresso@users.noreply.github.com>
2023-05-30 13:05:52 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,51 +1,53 @@
 function changeImage(element, newImage) {
   element.querySelector('img').src = newImage;
 }
+document.addEventListener('DOMContentLoaded', function() {
 
-const carouselContainer = document.querySelector('.carousel-container');
-const prevButton = document.querySelector('.prev-button');
-const nextButton = document.querySelector('.next-button');
 
-let currentIndex = 0;
-const images = carouselContainer.querySelectorAll('img');
-const totalImages = images.length;
-const imageWidth = images[0].clientWidth;
+  const carouselContainer = document.querySelector('.carousel-container');
+  const prevButton = document.querySelector('.prev-button');
+  const nextButton = document.querySelector('.next-button');
 
-function showImage(index) {
-  carouselContainer.style.transform = `translateX(-${index * imageWidth}px)`;
-}
+  let currentIndex = 0;
+  const images = carouselContainer.querySelectorAll('img');
+  const totalImages = images.length;
+  const imageWidth = images[0].clientWidth;
 
-prevButton.addEventListener('click', () => {
-  currentIndex = Math.max(currentIndex - 1, 0);
-  showImage(currentIndex);
-});
-
-nextButton.addEventListener('click', () => {
-  currentIndex = Math.min(currentIndex + 1, totalImages - 1);
-  showImage(currentIndex);
-});
-
-function replaceWithGame() {
-  var container = document.getElementById("container");
-  var gameUrl = "https://itch.io/embed-upload/7577153?color=de5ad0"; // Replace with the URL of the embedded game
-
-  var iframe = document.createElement("iframe");
-  iframe.src = gameUrl;
-  iframe.width = "320";
-  iframe.height = "500";
-
-  container.innerHTML = ""; // Clear the container
-  container.appendChild(iframe); // Append the iframe to the container
-}
-
-// Add this JavaScript code at the end of the body or in a separate script file
-const menuToggle = document.getElementById('menu-toggle');
-const menuLabel = document.querySelector('.menu-icon');
-
-menuToggle.addEventListener('change', function() {
-  if (menuToggle.checked) {
-    menuLabel.classList.add('open');
-  } else {
-    menuLabel.classList.remove('open');
+  function showImage(index) {
+    carouselContainer.style.transform = `translateX(-${index * imageWidth}px)`;
   }
+
+  prevButton.addEventListener('click', () => {
+    currentIndex = Math.max(currentIndex - 1, 0);
+    showImage(currentIndex);
+  });
+
+  nextButton.addEventListener('click', () => {
+    currentIndex = Math.min(currentIndex + 1, totalImages - 1);
+    showImage(currentIndex);
+  });
+
+  function replaceWithGame() {
+    var container = document.getElementById("container");
+    var gameUrl = "https://itch.io/embed-upload/7577153?color=de5ad0"; // Replace with the URL of the embedded game
+
+    var iframe = document.createElement("iframe");
+    iframe.src = gameUrl;
+    iframe.width = "320";
+    iframe.height = "500";
+
+    container.innerHTML = ""; // Clear the container
+    container.appendChild(iframe); // Append the iframe to the container
+  }
+
+  const menuToggle = document.getElementById('menu-toggle');
+  const menuLabel = document.querySelector('.menu-icon');
+
+  menuToggle.addEventListener('change', function() {
+    if (menuToggle.checked) {
+      menuLabel.classList.add('open');
+    } else {
+      menuLabel.classList.remove('open');
+    }
+  });
 });
